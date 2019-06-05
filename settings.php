a0fb5f26fdcb0b308c214c6b3f472e6e94b78872
Matt Porritt <mattp@catalyst-net.au>
2019-06-05 00:59:35 +0000

    code style fix

--- settings.php
+++ settings.php
@@ -18,7 +18,6 @@
  * Plugin administration pages are defined here.
  *
  * @package     tool_mdl63795
- * @category    admin
  * @copyright   2019 Matt Porritt <mattp@catalyst-au.net>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
